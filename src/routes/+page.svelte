f0512df825fbf992d3b159d85358d92b63c094e2
ikanexus <git+github@ika.gg>
2023-07-09 20:31:37 +0100

    add icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 	import { fade } from 'svelte/transition';
-  import { typewriter } from '@svelteuidev/motion';
+	import Icon from '@iconify/svelte';
+	import { typewriter } from '@svelteuidev/motion';
 	import Figure from '../components/Figure.svelte';
 	const links = [
 		{
 			name: 'GitHub',
-			href: 'https://github.com/ikanexus'
+			href: 'https://github.com/ikanexus',
+			icon: 'mdi:github'
 		}
 	];
 	const images = ['base.png', 'pestino.png'];
@@ -21,13 +23,17 @@
 </script>
 
 <Figure {images} />
-	<h1 class="text-4xl mx-auto pb-8" on:mouseenter={showName} on:mouseleave={hideName}>
-		Ika{#key visible}<span in:typewriter={{}} out:fade class={visible ? '' : 'hidden'}>Nexus</span>{/key}
-	</h1>
+<h1 class="text-4xl mx-auto pb-8" on:mouseenter={showName} on:mouseleave={hideName}>
+	Ika{#key visible}<span in:typewriter={{}} out:fade class={visible ? '' : 'hidden'}>Nexus</span
+		>{/key}
+</h1>
 <ul>
 	{#each links as link}
 		<li class="p-2">
-			<a href={link.href} class="btn btn-outline btn-lg btn-block">{link.name}</a>
+			<a href={link.href} class="btn btn-outline btn-lg btn-block">
+				<Icon icon={link.icon} />
+				{link.name}
+			</a>
 		</li>
 	{/each}
 </ul>
