1c0e6ba4b5661fc025621f39bc369b0bc25ffc23
ikanexus <git+github@ika.gg>
2023-07-09 18:05:37 +0100

    swap to sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	const links = [
+		{
+			name: 'GitHub',
+			href: 'https://github.com/ikanexus'
+		}
+	];
+</script>
+
+<figure class="p-8">
+	<img class="mask w-64 mask-circle mx-auto" src="/profile.png" alt="IkaNexus" />
+</figure>
+<h1 class="text-4xl pb-8">Ika</h1>
+<ul>
+{#each links as link}
+  <li class="p-2">
+    <a href={link.href} class="btn btn-outline btn-lg btn-block">{link.name}</a>
+  </li>
+{/each}
+</ul>
