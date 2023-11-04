c44a633f6822caf384828277c55365c2470a81fe
Ika <git+github@ika.gg>
2023-11-04 12:58:08 +0000

    move typewriter to css animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      mono: ["JetBrains Mono", "monospace"],
+    },
+    extend: {
+      animation: {
+        typewriter: "typewriter 1s steps(5) 1s 1 normal both",
+        blinkCursor: "blinkCursor 750ms steps(10) infinite normal",
+      },
+      keyframes: {
+        typewriter: {
+          from: { width: "0" },
+          to: { width: "7rem" },
+        },
+        blinkCursor: {
+          from: { borderRightColor: "currentColor" },
+          to: { borderRightColor: "transparent" },
+        },
+      },
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
