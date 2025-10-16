92705bf461d3b56093fc4f02e679f780665688f2
ikanexus <137009494+ikanexus@users.noreply.github.com>
2025-10-16 10:37:37 +0100

    convert astro profile component to svelte to better work with CSP

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,27 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [icon(), svelte()],
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    csp: {
+      directives: [
+        "default-src 'self'",
+        "font-src fonts.bunny.net",
+        "img-src 'self' data:",
+      ],
+      styleDirective: {
+        resources: [
+          "'self'",
+          "fonts.bunny.net"
+        ]
+      }
+    }
   }
 });
-
---
+++ src/components/Profile.svelte
@@ -0,0 +1,28 @@
+<script>
+import { onMount } from 'svelte';
+import base from '../profiles/base.png';
+import pestino from '../profiles/pestino.png';
+
+const images = [base, pestino];
+let currentIndex = 0;
+
+onMount(() => {
+	const interval = setInterval(() => {
+		currentIndex = (currentIndex + 1) % images.length;
+	}, 7500);
+	return () => clearInterval(interval);
+});
+</script>
+
+<div class="mx-auto relative h-64 w-64 my-8">
+	{#each images as image, index}
+		<img
+			type="image"
+			alt=""
+			class={`absolute top-0 left-0 h-full w-full mask mask-circle bg-center bg-no-repeat ${
+index === currentIndex ? 'opacity-100' : 'opacity-0'
+}`}
+			src={image.src}
+		/>
+	{/each}
+</div>
